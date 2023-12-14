bd34bb448a6e9e812e0183b4724205c9e966814d
Juan Camilo Noriega Hoyos <jcnorieg@bancolombia.com.co>
2023-12-14 15:44:19 -0500

    cargando el proyecto

---
+++ src/main/java/com/creacionjuan/sqsykafkasqlprestamos/SqsykafkasqlprestamosApplication.java
@@ -0,0 +1,13 @@
+package com.creacionjuan.sqsykafkasqlprestamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsykafkasqlprestamosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsykafkasqlprestamosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creacionjuan/sqsykafkasqlprestamos/SqsykafkasqlprestamosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creacionjuan.sqsykafkasqlprestamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsykafkasqlprestamosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
